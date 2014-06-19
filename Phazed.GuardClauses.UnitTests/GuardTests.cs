c8500ded8bcc33bfd8b32f843e7c3a0047451ccf
Rupert Benbrook <rupert@phazed.com>
2014-06-19 11:31:28 +0100

    Initial commit with basic implementation. Needs filling out and ensuring PCL is as widely targetted as possible.

---
+++ Phazed.GuardClauses.UnitTests/GuardTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Phazed.GuardClauses.UnitTests
+{
+    [TestFixture]
+    public class GuardTests
+    {
+        [Test]
+        public void AgainstNull_Null_ThrowsArgumentNullException()
+        {
+            Assert.That(
+                () => Guard.AgainstNull(null, "param"),
+                Throws.TypeOf<ArgumentNullException>().With.Property("ParamName").EqualTo("param")
+                    .And.Property("Message").StartsWith("Value cannot be null."));
+        }
+
+        [Test]
+        public void AgainstNull_NotNull_DoesNotThrow()
+        {
+            Assert.That(
+                () => Guard.AgainstNull(new object(), "param"),
+                Throws.Nothing);
+        }
+
+        [Test]
+        public void AgainstEmpty_EmptyString_ThrowsArgumentException()
+        {
+            Assert.That(
+                () => Guard.AgainstEmpty("", "param"),
+                Throws.TypeOf<ArgumentException>().With.Property("ParamName").EqualTo("param")
+                    .And.Property("Message").StartsWith("Value cannot be empty."));
+        }
+
+        [Test]
+        public void AgainstEmpty_NullString_ThrowsArgumentException()
+        {
+            Assert.That(
+                () => Guard.AgainstEmpty(null, "param"),
+                Throws.TypeOf<ArgumentNullException>().With.Property("ParamName").EqualTo("param")
+                    .And.Property("Message").StartsWith("Value cannot be null."));
+        }
+
+        [Test]
+        public void AgainstEmpty_NonEmptyString_DoesNotThrow()
+        {
+            Assert.That(
+                () => Guard.AgainstEmpty("notempty", "param"),
+                Throws.Nothing);
+        }
+    }
+}
---
+++ Phazed.GuardClauses/Guard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Phazed.GuardClauses
+{
+    public static class Guard
+    {
+        public static void AgainstNull(object value, string paramName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+
+        public static void AgainstEmpty(string value, string paramName)
+        {
+            AgainstNull(value, paramName);
+            if (value.Length == 0)
+            {
+                throw new ArgumentException("Value cannot be empty.", paramName);
+            }
+        }
+    }
+}
